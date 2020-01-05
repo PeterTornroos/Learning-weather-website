3e55ed7110c3e28a4095e519211bcd7c8461862b
PeterTornroos <peter.tornroos@gmail.com>
2020-01-05 17:54:38 +0200

    Added high and low temp info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = function(lat, long, callback) {
         } else if (body.error) {
             callback("Unable to find location", undefined)
     } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.")
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain. Highest temeperature for the day is " + body.daily.data[0].temperatureHigh + " degrees and lowest temperature is " + body.daily.data[0].temperatureLow + " degrees.")
         }
     })
 }
